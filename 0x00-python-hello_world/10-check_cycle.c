c11a08a35da963ff4978a4b2d5cc5818b62f6fbf
Osama Elsharqawy <elsharqawy2@gmail.com>
2024-01-01 09:48:14 -0800

    Hello World

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if the list has a cycle
+ * @head: pointer to list to be checked
+ * Return: void
+*/
+int check_cycle(listint_t *head)
+{
+	listint_t *newHead = head;
+	listint_t *next = head->next;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	while (next != NULL && next->next != NULL)
+	{
+		if (newHead == next)
+			return (1);
+		newHead = newHead->next;
+		next = next->next->next;
+	}
+	return (0);
+}
