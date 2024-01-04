dbfa2b9cc5826da650ef5bdec8402be9da3e42f9
Osama Elsharqawy <elsharqawy2@gmail.com>
2024-01-04 10:29:28 -0800

    Python import args

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Prints the result of the addition of all arguments."""
+    import sys
+
+    args = sys.argv[1:]
+    num = 0
+    for arg in args:
+    	num += int(arg)
+    print("{:d}".format(num))
