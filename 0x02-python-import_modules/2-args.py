8de3f7fb931905abcb3a5fd8e2f22f18107cdc86
Osama Elsharqawy <elsharqawy2@gmail.com>
2024-01-04 10:20:07 -0800

    Python import args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	"""Prints the number of and the list of its arguments."""
 	import sys
 
 	args = len(sys.argv) - 1
 	if args == 0:
-		print("0 argument.")
+		print("0 arguments.")
 	elif args == 1:
 		print("1 argument:")
 	else:
