13333015ace653534a4156c042d0883275e90e17
Osama <elsharqawy2@gmail.com>
2024-01-09 00:08:51 +0300

    Python ds

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    if my_list == []:
+        return None
+    newList = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            newList.append(True)
+        else:
+            newList.append(False)
+    return newList
