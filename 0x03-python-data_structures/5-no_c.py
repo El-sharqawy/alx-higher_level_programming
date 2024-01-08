53503f69d6b76d9f059885202d461b87d2606f86
Osama <elsharqawy2@gmail.com>
2024-01-08 19:49:32 +0300

    Python ds

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,11 @@
 def no_c(my_string):
     new_string = ""
     for i in my_string:
-        if i != "c":
+        if i != "c" and i != "C":
             new_string += i
 
     return new_string
+
+print(no_c("Best School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
