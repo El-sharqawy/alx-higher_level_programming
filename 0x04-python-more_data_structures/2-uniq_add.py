dd6f93d563d20c08b9edb34194ada29b4a7f8dc5
Osama <elsharqawy2@gmail.com>
2024-01-10 17:33:08 +0300

    Python more ds

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
-def uniq_add(my_list):
+def uniq_add(my_list=[]):
     sum = 0
     sum_set = set()
     for item in my_list:
-        if item not in my_list:
+        if item not in sum_set:
             sum += item
-            sum_set.add(sum)
-    return sum_set
+            sum_set.add(item)
+    return sum
+
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
