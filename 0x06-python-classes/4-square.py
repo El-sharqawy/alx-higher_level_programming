230f11ba7d30157347853724f5d79598929ebb3f
Osama <elsharqawy2@gmail.com>
2024-01-24 00:57:14 +0300

    Python Classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,11 +5,6 @@
 class Square():
     """ The intialization of Square class"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     def size(self):
