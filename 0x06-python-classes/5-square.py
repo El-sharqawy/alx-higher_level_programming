976a31866f09f0f684d19f37797038706128b029
Osama Elsharqawy <elsharqawy2@gmail.com>
2024-01-23 14:27:02 -0800

    DAMN INTERNET

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -28,5 +28,5 @@ class Square():
         if self.__size == 0:
             print("")
         for i in range(self.__size):
-            print("#" * self.__size):
+            print("#" * self.__size)
 
