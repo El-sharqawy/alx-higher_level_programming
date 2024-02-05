1ac3029c097e852331028de4497d507a445e6aaa
Osama Elsharqawy <elsharqawy2@gmail.com>
2024-02-05 07:05:11 -0800

    Python inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a Rectangle subclass named Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ Square Class"""
+
+    def __init__(self, size):
+        """ Initialize square class"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
