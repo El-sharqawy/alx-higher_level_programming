d3b7b43cdb540b7755f94fd9f0587baabd4b7701
Osama Elsharqawy <elsharqawy2@gmail.com>
2024-02-05 06:21:30 -0800

    Python inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-a function that returns True if the object is an instance,
-of a class that inherited (directly or indirectly),
-from the specified class ; otherwise False.
+only sub class of
 """
 
 
