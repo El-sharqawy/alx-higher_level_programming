17048d8891532058fd64712b044e7d12917b78fb
Osama Elsharqawy <elsharqawy2@gmail.com>
2024-02-06 12:15:32 -0800

    Python Input/Output Projects

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Write to a file
+"""
+
+def write_file(filename="", text=""):
+    """ a function that writes to a file"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        chars_written = file.write(text)
+        return chars_written
