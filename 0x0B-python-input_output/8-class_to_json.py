0f4c6cebcf9af55c1fac3381d88834623f6fa3dd
Osama Elsharqawy <elsharqawy2@gmail.com>
2024-02-06 15:35:24 -0800

    Python Input/Output Projects

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,6 +6,7 @@ Class to JSON
 
 def class_to_json(obj):
     """ a function that returns dict of obj"""
-    result = if hasattr(obj, "__dict__"):
+    result = {}
+    if hasattr(obj, "__dict__"):
         result = obj.__dict__.copy()
     return (result)
