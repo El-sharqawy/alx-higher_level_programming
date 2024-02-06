7346d51b33fb9615f15b31eea8b9d03bb28a6e38
Osama Elsharqawy <elsharqawy2@gmail.com>
2024-02-06 15:08:52 -0800

    Python Input/Output Projects

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,10 +3,11 @@
 Load, add, save function
 """
 import sys
-stjf = __import__('5-save_to_json_file').save_to_json_file
-lfjf = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
+    stjf = __import__('5-save_to_json_file').save_to_json_file
+    lfjf = __import__('6-load_from_json_file').load_from_json_file
+
     try:
         items = lfjf("add_item.json")
     except FileNotFoundError:
