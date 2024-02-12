d9b6b80dec00fcdbcd3e2d6431ec9ae4e2daee52
Osama <elsharqawy2@gmail.com>
2024-02-12 18:21:48 +0200

    Python circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -80,7 +80,6 @@ class Base:
                 for obj in list_objs:
                     writer.writerow(obj.to_dictionary())
 
-
     @classmethod
     def load_from_file_csv(cls):
         """Return a list of classes instantiated from a CSV file."""
