145cf5c266409752ae7c93273e1853bc7813bab2
Osama <elsharqawy2@gmail.com>
2024-04-08 21:38:28 +0200

    Hello Javascript!

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const X = parseInt(process.argv[2]);
+if (isNaN(X)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < X; i++) {
+    for (let j = 0; j < X; j++) {
+      process.stdout.write('X');
+      if (j === X-1) {
+        process.stdout.write('\n');
+      }
+    }
+  }
+}
