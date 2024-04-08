603566ca87ab05a7434e6bd8856ce53072c3f445
Osama <elsharqawy2@gmail.com>
2024-04-08 20:46:50 +0200

    Hello Javascript!

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (process.argv.length == 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
