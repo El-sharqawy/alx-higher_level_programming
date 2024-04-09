8b5a8a9cffa3600c0cbfe74f15bd4b3cb6178bc2
Osama <elsharqawy2@gmail.com>
2024-04-09 22:02:47 +0200

    Hello Javascript!

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,6 +4,9 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
+    if (!size || size <= 0) {
+      return;
+    }
     super(Rectangle);
     this.width = size;
     this.height = size;
