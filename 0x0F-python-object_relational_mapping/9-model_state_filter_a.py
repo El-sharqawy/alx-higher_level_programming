2e30e64efd52ab0238d71d0c503d2419d6260e39
Osama <elsharqawy2@gmail.com>
2024-04-15 00:49:03 +0200

    Python MySQLdb

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+script to list all State objects from hbtn_0e_6_usa
+Results must be in ASC Order based on ID
+"""
+
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def createSession(username, password, database):
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).filter(State.name.like('%a%')):
+        print(state.id, state.name, sep=": ")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("""Usage: ./7-model_state_fetch_all.py <user> <pwd> <db>""")
+        sys.exit(1)
+
+    createSession(sys.argv[1], sys.argv[2], sys.argv[3])
