49bb5f9435b6ef057bfbf1d17a8b9fe4b9dec356
Osama <elsharqawy2@gmail.com>
2024-04-15 00:04:46 +0200

    Python MySQLdb

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Class definition of State"""
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+metaData = MetaData()
+Base = declarative_base()
+
+
+class State(Base):
+    """Class with id and name for states"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullablle=False)
