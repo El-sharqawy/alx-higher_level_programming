4e6d5598b6b37e4611aac5aa12868df8de536c45
Osama <elsharqawy2@gmail.com>
2024-04-15 00:58:42 +0200

    Python MySQLdb

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+script to list all State objects from hbtn_0e_6_usa
+Results must be in ASC Order based on ID
+"""
+
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def createSession(username, password, database):
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    newState = State(name='Louisiana')
+    session.add(newState)
+    state = session.query(State).filter_by(name='Louisiana').first()
+
+    print(state.id)
+    session.commit()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print("""Usage: ./11-model_state_insert.py <user> <pwd> <db>""")
+        sys.exit(1)
+
+    createSession(sys.argv[1], sys.argv[2], sys.argv[3])
