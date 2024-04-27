c0fad40165f685e8f31945089330ddd1804a98dc
Osama <elsharqawy2@gmail.com>
2024-04-27 16:43:24 +0300

    Python Network II

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,9 +7,9 @@ import sys
 
 
 url = sys.argv[1]
-try:
-    response = requests.get(url)
-    response.raise_for_status()
+response = requests.get(url)
+response.raise_for_status()
+if response.status_code >= 400:
+    print(f"Error code: {response.status_code}")
+else:
     print(f"{response.text}")
-except requests.HTTPError as err:
-    print(f"Error code: {err.response.status_code}")
