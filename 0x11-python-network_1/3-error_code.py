59f17c008b0c952ba095b909fbb48a3c0a405bdb
Osama <elsharqawy2@gmail.com>
2024-04-27 16:21:35 +0300

    Python Network II

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a Python script that takes URL, sends
+    a request to it and displays the body
+    of the response (decoded in utf8)
+"""
+import sys
+import urllib.request
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.request.HTTPError as err:
+        print(f"Error Code: {err.code}")
