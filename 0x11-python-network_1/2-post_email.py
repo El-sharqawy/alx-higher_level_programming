6b65c315659ac6f6f3018276130ac6ec62cfa446
Osama <elsharqawy2@gmail.com>
2024-04-27 16:11:46 +0300

    Python Network II

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     email = sys.argv[2]
 
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    request = urllib.request.Request(url, data = data, method='POST')
+    request = urllib.request.Request(url, data=data, method='POST')
     with urllib.request.urlopen(request) as response:
         body = response.read().decode('utf-8')
         print(f"Your email is: {body}")
