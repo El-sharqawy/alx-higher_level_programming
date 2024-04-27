6084682984b2076ecef36b1320f8be9f5adc1c6b
Osama <elsharqawy2@gmail.com>
2024-04-27 16:23:07 +0300

    Python Network II

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
             body = response.read().decode('utf-8')
             print(body)
     except urllib.request.HTTPError as err:
-        print(f"Error Code: {err.code}")
+        print(f"Error code: {err.code}")
