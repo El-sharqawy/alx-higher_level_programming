f9dac4c4f390f2e02b207a08c521a3a21a6d3033
El-sharqawy <elsharqawy2@gmail.com>
2024-05-21 16:50:14 +0300

    FEEL THE POWER

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('node:fs');
-const fileName = process.argv[2]
-const content = process.argv[3]
+const fileName = process.argv[2];
+const content = process.argv[3];
 fs.writeFileSync(fileName, content, { encoding: 'utf8', flag: 'w' }, (err) => {
   if (err) throw err;
 });
